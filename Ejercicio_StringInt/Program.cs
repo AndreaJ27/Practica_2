bb6ebf495cb6a956f319b2805345a350efed5a57
Josefina Arreola <maya_aiji.11@hotmail.com>
2021-03-02 19:29:24 -0800

    Ejercicio String e Int, impresión de los datos ingresados

---
+++ Ejercicio_StringInt/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_StringInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            String nombre;
+            int edad;
+            String linea;
+
+            //Operación
+            Console.WriteLine("Ingresa tu nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingresa tu edad: ");
+            linea = Console.ReadLine();
+            edad = int.Parse(linea);
+
+            //Resultado
+            Console.WriteLine("Te llamas " + nombre + " y tienes " + edad);
+            Console.WriteLine("Presiona cualquier tecla para continuar");
+            Console.ReadKey();
+
+        }
+    }
+}
