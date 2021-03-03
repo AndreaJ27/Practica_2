3acdbb496ad6fdf56dce19aeb4839f2eee9af1f9
Josefina Arreola <maya_aiji.11@hotmail.com>
2021-03-02 18:57:18 -0800

    Declaración de variables y operación para sacar un perimetro

---
+++ CalculoPerimetro/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoPerimetro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Variables
+            int a = 10;
+            int b = 10;
+            int c = 7;
+            int p;
+
+            //Operación
+
+            p = a + b + c;
+
+            //Resultado
+
+            Console.WriteLine("El perimetro del triangulo es: " + p);
+            Console.WriteLine("Presiona cualquier tecla para continuar");
+            Console.ReadKey();
+
+        }
+    }
+}
