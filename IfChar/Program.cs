feaa7002af7457c1f56cd55c6675d9c1067c2f0c
Josefina Arreola <maya_aiji.11@hotmail.com>
2021-03-18 18:42:02 -0700

    Ejemplo IfChar

---
+++ IfChar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Operación 
+
+            Console.WriteLine("Ingresa algun caracter: ");
+            char c = (char)Console.Read();
+
+            if (Char.IsLetter(c))
+            {
+                if(Char.IsLower(c))
+                {
+                    Console.WriteLine("El caracter es letra minuscula");
+                } 
+                
+                else
+                 {
+                    Console.WriteLine("El caracter es letra mayscula");
+                 }
+             
+            }
+
+            else
+             {
+                Console.WriteLine("El caracter es un numero");
+             }
+
+            Console.WriteLine("Presione cualquier tecla para continuar");
+            Console.ReadKey();
+
+        }
+    }
+}
